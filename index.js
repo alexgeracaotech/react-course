efde73f9d1011334677080ceebeb2d776c25bf9d
Alex Bessa <alexbessa.dev@gmail.com>
2025-09-06 11:04:49 -0300

    iniciando servidor para refresh de arquivo em react

---
+++ index.js
@@ -0,0 +1,13 @@
+
+const container = document.querySelector('#container');
+
+const element = () => {
+  return (
+    <div>
+      <h1>Hello World!</h1>
+      <p>Welcome to React!</p>
+    </div>
+  );
+};
+
+ReactDOM.createRoot(container).render(element());
